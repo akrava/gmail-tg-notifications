3e92b88695fd4c7ca98a5802bc85cb93fa6f1035
Arkadiy <arkadiy@ex.ua>
2020-08-02 13:38:47 +0300

    fix typo in help command

--- src/telegram/commands/help.ts
+++ src/telegram/commands/help.ts
@@ -4,7 +4,7 @@ const help: Middleware<Context> = async function(ctx) {
     ctx.reply(
         "Tap /start to get started.\n" +
         "Tap /connect_gmail to subcribe for new emails.\n" +
-        "Enter chats ID to send greetings in such format:\n" +
+        "Enter chats ID to send emails in such format:\n" +
         "***\n" +
         "/set_chats\n" +
         "xxxx xxxx xxxx xxxx\n" +
