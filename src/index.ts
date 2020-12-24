6b1bb3577c1ff878d9ac50ec6643bcbe4fa61bb0
Arkadiy <arkadiy@ex.ua>
2020-12-25 01:04:04 +0200

    fix dependencies issue

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ const connectionsOptions = {
     useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true
 };
 
-Mongoose.connect(process.env.DB_URL, connectionsOptions,
-        (err) => err ? error(err) : info("Opened connection with db")
-    ).then(() => ServerApp.listen(process.env.PORT, () => info(
+Mongoose.connect(process.env.DB_URL, connectionsOptions)
+    .catch((err) => err ? error(err) : info("Opened connection with db"))
+    .then(() => ServerApp.listen(process.env.PORT, () => info(
         `Running on port ${process.env.PORT}`
     ))).then(() => TelegramBot.telegram.setWebhook(
         url.resolve(process.env.SERVER_PATH, process.env.WEBHOOK_TG_PATH
