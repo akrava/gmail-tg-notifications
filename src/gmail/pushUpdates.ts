c824083a7eabbfa95eed918e32295b3a782e9614
Arkadiy <arkadiy@ex.ua>
2020-12-25 01:50:15 +0200

    another check in prod

--- src/gmail/pushUpdates.ts
+++ src/gmail/pushUpdates.ts
@@ -11,19 +11,19 @@ const authClient = new OAuth2Client();
 export const router = Express.Router();
 
 router.post(process.env.GAPPS_PUSH_PATH, jsonBodyParser, async (req, res) => {
+    try {
+        const bearer = req.header("Authorization");
+        const [, token] = bearer.match(/Bearer (.*)/);
+        await authClient.verifyIdToken({
+            idToken: token,
+            audience: process.env.SERVER_PATH.replace(/https?:\/\/|\//g, ""),
+        });
+    } catch (e) {
+        error(e);
+        res.status(400).send("Invalid token");
+        return;
+    }
     res.send(500);
-    // try {
-    //     const bearer = req.header("Authorization");
-    //     const [, token] = bearer.match(/Bearer (.*)/);
-    //     await authClient.verifyIdToken({
-    //         idToken: token,
-    //         audience: process.env.SERVER_PATH.replace(/https?:\/\/|\//g, ""),
-    //     });
-    // } catch (e) {
-    //     error(e);
-    //     res.status(400).send("Invalid token");
-    //     return;
-    // }
     // const message = Buffer.from(req.body.message.data, "base64").toString("utf-8");
     // const obj = JSON.parse(message);
     // const user = await FindUserByEmail(obj.emailAddress);
