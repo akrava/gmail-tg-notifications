d2ffaf950cb4adfd52a9c0b136ac0fc977985f0e
Arkadiy <arkadiy@ex.ua>
2020-12-25 13:38:16 +0200

    refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,14 @@ const connectionsOptions = {
     useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true
 };
 
+const SERVER_PATH = process.env.SERVER_PATH;
+const WEBHOOK_TG_PATH = process.env.WEBHOOK_TG_PATH;
+const PORT = process.env.PORT;
+const completeWebhookTgPath = url.resolve(SERVER_PATH, WEBHOOK_TG_PATH);
+
 Mongoose.connect(process.env.DB_URL, connectionsOptions)
-    .catch((err) => err ? error(err) : info("Opened connection with db"))
-    .then(() => ServerApp.listen(process.env.PORT, () => info(
-        `Running on port ${process.env.PORT}`
-    ))).then(() => TelegramBot.telegram.setWebhook(
-        url.resolve(process.env.SERVER_PATH, process.env.WEBHOOK_TG_PATH
-    ))).then((res) => res ? info("Tg bot started") : error(new Error("webhook error")))
-    .catch((err) => error(err));
+    .catch(err => err ? error(err) : info("Opened connection with db"))
+    .then(() => ServerApp.listen(PORT, () => info(`Running on port ${PORT}`)))
+    .then(() => TelegramBot.telegram.setWebhook(completeWebhookTgPath))
+    .then(res => res ? info("Tg bot started") : error(new Error("webhook error")))
+    .catch(err => error(err));
