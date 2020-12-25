f89a0e655b4a39883c23b2bb567d81296ac68661
Arkadiy <arkadiy@ex.ua>
2020-12-25 15:43:36 +0200

    refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,17 @@ import Mongoose from "mongoose";
 import url from "url";
 
 const connectionsOptions = {
-    useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true
+    useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false,
+    useUnifiedTopology: true
 };
 
 const SERVER_PATH = process.env.SERVER_PATH;
 const WEBHOOK_TG_PATH = process.env.WEBHOOK_TG_PATH;
 const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 const completeWebhookTgPath = url.resolve(SERVER_PATH, WEBHOOK_TG_PATH);
 
-Mongoose.connect(process.env.DB_URL, connectionsOptions)
+Mongoose.connect(DB_URL, connectionsOptions)
     .catch(err => err ? error(err) : info("Opened connection with db"))
     .then(() => ServerApp.listen(PORT, () => info(`Running on port ${PORT}`)))
     .then(() => TelegramBot.telegram.setWebhook(completeWebhookTgPath))
