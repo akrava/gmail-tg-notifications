5840ab4962e9a2a30fee6737d7a18e1f491380ec
Arkadiy <arkadiy@ex.ua>
2020-12-26 22:35:38 +0200

    fix help message

--- src/telegram/commands/help.ts
+++ src/telegram/commands/help.ts
@@ -2,35 +2,34 @@ import { Context, Middleware } from "telegraf";
 import { BotCommand } from "telegraf/typings/telegram-types";
 
 const help: Middleware<Context> = async function(ctx) {
-    ctx.reply(
+    ctx.replyWithHTML(
         "Tap /start to get started.\n" +
         "Tap /connect_gmail to subcribe for new emails.\n" +
         "To forward emails from gmail into specific chats " +
         "or channels you should enter chats ID in such format:\n" +
-        "```\n" +
+        "<code>\n" +
         "/set_chats\n" +
         "xxxx xxxx xxxx xxxx\n" +
-        "```\n" +
+        "</code>\n" +
         "Use /filter_emails command to filter incoming mails from senders email " +
         "addressess.\nTo set filter rule to block only emails from specified " + 
         "senders you should type such message:\n" +
-        "```\n" +
+        "<code>\n" +
         "/filter_emails\n" +
         "block\n" +
         "john@example.com tom@simple.org" +
-        "```\n" +
+        "</code>\n" +
         "To set filter rule to allow only emails from specified " + 
         "senders you should type such message:\n" +
-        "```\n" +
+        "<code>\n" +
         "/filter_emails\n" +
         "allow\n" +
         "john@example.com tom@simple.org\n" +
-        "```\n" +
+        "</code>\n" +
         "Tap /delete_token to unsubscribe from gmail updates and delete creds.\n" +
         "Tap /delete_profile to unsubscribe, delete creds and profile from DB.\n" +
         "Chats or channels id you can get here: @userinfobot.\n" +
-        "Tap /get_id to get id of group chat.",
-        { parse_mode: "Markdown" }
+        "Tap /get_id to get id of group chat."
     );
 };
 
