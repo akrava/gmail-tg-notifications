d8ac469effc13a2e22acf3ee2e4b9441f8a45ed9
Arkadiy <arkadiy@ex.ua>
2021-02-10 11:38:13 +0200

    remove logs

--- src/gmail/index.ts
+++ src/gmail/index.ts
@@ -171,13 +171,10 @@ export async function getEmails(emailAdress: string, historyId: number): Promise
     const result = [];
     for (const mail of messagesDocuments) {
         let message = "";
-        console.log(mail.payload.parts);
         if (mail.payload.parts) {
             let data = mail.payload.parts.filter((x) => x.mimeType.includes("text/html"));
             if (data.length === 0) {
                 for (const part of mail.payload.parts) {
-                    console.log("level 1");
-                    console.log(part.parts);
                     if (part.parts) {
                         data = data.concat(part.parts.filter((x) => x.mimeType.includes("text/html")));
                     }
@@ -188,7 +185,6 @@ export async function getEmails(emailAdress: string, historyId: number): Promise
 
             // TODO 
             if (message.trim().length === 0) {
-                console.log("level 2");
                 let data = mail.payload.parts.filter((x) => x.headers && x.headers.filter(x => x.name && x.name.includes("Content-Type") && x.value && x.value.includes("text/html")).length > 0);
                 if (data.length === 0) {
                     for (const part of mail.payload.parts) {
@@ -200,12 +196,8 @@ export async function getEmails(emailAdress: string, historyId: number): Promise
                 message = data.reduce((prev, cur) => prev += base64ToString(cur.body.data), ""); //
                 message = htmlToText.fromString(message); //
             }
-            console.log("level 3");
-            mail.payload.parts.forEach(x => console.log(x));
             // TODO 
         } else if (mail.payload.body) {
-            console.log("level4");
-            console.log(mail.payload.body);
             message = htmlToText.fromString(base64ToString(mail.payload.body.data || ""));
         }
         if (mail.payload.headers) {
